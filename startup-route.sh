0af8ba065a31bebd3a6af8aa115b137e794f7d48
Artem Semenov <artem@semenov.ws>
2020-03-01 21:42:20 +0300

    some commit

--- startup-route.sh
+++ startup-route.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 router="10.8.0.1"
+tmp="/private/var/tmp/net.vpn.route"
 
-curl -z tmp/subnet.lst https://antifilter.download/list/subnet.lst --output tmp/subnet.lst
-curl -z tmp/ipsum.lst https://antifilter.download/list/ipsum.lst --output tmp/ipsum.lst
+[ ! -d $tmp ] && mkdir $tmp
+curl -z $tmp/subnet.lst https://antifilter.download/list/subnet.lst --output $tmp/subnet.lst
+curl -z $tmp/ipsum.lst https://antifilter.download/list/ipsum.lst --output $tmp/ipsum.lst
 
-old=$(cat tmp/md5);
-new=$(cat tmp/*.lst | md5sum | head -c 32);
+old=$(cat $tmp/md5);
+new=$(cat $tmp/*.lst | md5sum | head -c 32);
 
 if [ "$old" != "$new" ]
 then
-cat tmp/ipsum.lst > tmp/ip.list
-cat tmp/subnet.lst >> tmp/ip.list
-for ip in $(grep -Ev '^$|^#' tmp/ip.list)
+cat $tmp/ipsum.lst > $tmp/ip.list
+cat $tmp/subnet.lst >> $tmp/ip.list
+for ip in $(grep -Ev '^$|^#' $tmp/ip.list)
 do
 route add $ip $router 1>/dev/null 
 done
 
-echo $new > tmp/md5
+echo $new > $tmp/md5
 fi
